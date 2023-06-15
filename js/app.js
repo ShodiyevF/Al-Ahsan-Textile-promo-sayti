2f93ad766a83d099925469b06e9b236d12b7aa68
ShodiyevF <shodiyevf@yandex.ru>
2023-06-15 21:42:42 +0500

    add java script

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert (`Salom!`) 
+
+let name = prompt("Kim bu o'zi?")
+alert (`Salom ${name}!`)
+
+let age = prompt(`Iltimos yoshingizni oxiriga yosh so'zini qo'shib kiriting! `)
+
+let birthday = prompt (`Oladigan mahsulotingizni nomini yozing.`)
+
+alert(`Rahmat! Thank You! Спасибо!`)
+
+console.log (name, age ,birthday)
