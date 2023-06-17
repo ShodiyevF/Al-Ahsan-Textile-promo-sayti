50bd57774943c955a2d4e373ed9cc8e841788495
ShodiyevF <shodiyevf@yandex.ru>
2023-06-17 14:18:31 +0500

    login

--- js/login.js
+++ js/login.js
@@ -11,3 +11,61 @@ username.addEventListener('click', (e) => {
     username.value = ''; // Clear the input value
     console.log(valueToLog); // Log the stored value
   });
+
+  
+
+  const body = document.querySelector('body')
+const container = document.querySelector('.container')
+const colorText = document.querySelector('.color-text')
+const copyMe = document.querySelector('.copyme')
+
+const values = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f'
+]
+
+function getGradient() {
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        const randomNumber = Math.trunc(Math.random() * values.length)
+        color += values[randomNumber]
+    }
+
+    return color
+}
+
+function setGradient() {
+    const color1 = getGradient()
+    const color2 = getGradient()
+    const randomDeg = Math.trunc(Math.random() * 360)
+    const bgColor = `linear-gradient(
+        ${randomDeg}deg,
+        ${color1},
+        ${color2}
+    )`
+
+    body.style.background = bgColor
+    colorText.textContent = bgColor
+
+    copyMe.addEventListener('click', (e) => {
+        navigator.clipboard.writeText(bgColor)
+        alert(`Copy bo'ldi `)
+    })
+
+}
+
+setGradient()
