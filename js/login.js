2f3af66d94a6e9f117a211a9d309992e93b192e9
ShodiyevF <shodiyevf@yandex.ru>
2023-06-17 14:15:59 +0500

    Add some java script

--- js/login.js
+++ js/login.js
@@ -5,3 +5,9 @@ signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log(username.value)
 })
+
+username.addEventListener('click', (e) => {
+    const valueToLog = username.value; // Store the value before clearing
+    username.value = ''; // Clear the input value
+    console.log(valueToLog); // Log the stored value
+  });
